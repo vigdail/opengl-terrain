d3a879e85024baba424e55c775e9173e922676b7
Константин Бедрицкий <kmb2@tpu.ru>
2021-01-21 23:19:17 +0600

    Directional light. Redundant constructor removed

--- src/light/directional_light.h
+++ src/light/directional_light.h
@@ -5,7 +5,6 @@
 class DirectionalLight {
  public:
   DirectionalLight(glm::vec3 position, glm::vec3 look_at);
-  explicit DirectionalLight(glm::vec3 direction);
   glm::vec3 GetDirection();
   void SetPosition(glm::vec3 position);
   glm::vec3 GetColor();
